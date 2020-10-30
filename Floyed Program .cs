b4aba05102554e86d59ef4de0c983a3df3ca1e5c
Sarah Stepak <47461547+dusteenie@users.noreply.github.com>
2020-10-29 23:36:02 -0400

    Update Floyed Program .cs
    Fixed a lot of bugs in createGraph() and floyd()

--- Floyed Program .cs
+++ Floyed Program .cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Reflection;
 
 namespace GraphFloyd
 {
@@ -7,28 +8,26 @@ namespace GraphFloyd
      * GraphFloyd is a program which simulates the Floyd Algorithem. 
      * 
      * @author Dr. Gail Lange
-     * @author Sarah Stepak
-     * @see https://drive.google.com/drive/u/2/folders/1kL54gaSDDU5xCsQjjwcAYdrp8pAQZX0d
-     *     The following link contains the data for the program
-     * @since   10/28/2020
+     * @author Dusty Stepak
+     * @since   10/29/2020
      */
     class GraphFloyd
     {
         public const int INFINITY = 30000;
         public static int[,] adjacency;         // The adjacency matrix
         public static int numVertex;             // Number of vertices
-        private static int[,] D;
-        private static int[,] P;
+        public static int[,] D;
+        public static int[,] P;
 
 
         /*
          * Floyd() is a method that parses through arrays D and P to simulate
          * the Floyd algorithem
          * 
-         * @author  Sarah Stepak
+         * @author  Dusty Stepak
          * @return  void
          */
-        public static void Floyd() 
+        public static void Floyd()
         {
             /*
              * Declares and initalizes variables D,P,len. 
@@ -40,41 +39,76 @@ namespace GraphFloyd
              *          
              * @Var len   int     Contains the length of the adjacency array
              */
-            int[,] D = new int[adjacency.GetLength(0), adjacency.GetLength(1)];
-            int[,] P = new int[adjacency.GetLength(0), adjacency.GetLength(1)];
 
-            // Initalizes D and P
-            for (int r = 0; r < adjacency.GetLength(0); r++) 
-            {
-                for(int c = 0; c < adjacency.GetLength(1); c++) 
-                {
-                    // Sets D's values equal to the the adjacency array
-                    // initalizes P to -1
-                    D[r, c] = adjacency[r, c];
+            // Gail added the following two lines
+            D = new int[numVertex, numVertex];
+            P = new int[numVertex, numVertex];
+
+
+            // The following initalizes D and P   
+
+            // P is initalized to -1
+            for (int r = 0; r < numVertex; r++)
+                for (int c = 0; c < numVertex; c++)
                     P[r, c] = -1;
-                }
-            }
+
+            // Sets D to the adjacency array
+            for (int r = 0; r < numVertex; r++)
+                for (int c = 0; c < numVertex; c++)
+                    D[r, c] = adjacency[r, c];
+
 
             // Floyd algorithem
-            for (int k = 0; k < numVertex; k++) 
+            for (int k = 0; k < numVertex; k++)
             {
-                for (int i = 0; i < numVertex; i++) 
+                for (int i = 0; i < numVertex; i++)
                 {
-                    for(int j = 0; j < numVertex; j++)
+                    for (int j = 0; j < numVertex; j++)
                     {
-                        D[i, j] = Math.Min(D[i, j], (D[i, k] + D[k, j]));  // Sets D[i,j] to the shortest path
+                        if ((D[i, k] + D[k, j]) < D[i, j])
+                        {
+                            D[i, j] = D[i, k] + D[k, j];
+                            P[i, j] = k;
+                        }
                     }
                 }
             }
 
+            // Outputs the final D matrix
+            Console.WriteLine("\nFinal D Matrix: ");
+            for (int r = 0; r < D.GetLength(0); r++)
+            {
+                for (int c = 0; c < D.GetLength(1); c++)
+                {
+                    if (D[r, c] == INFINITY)
+                        Console.Write(" oo");
+                    else
+                        Console.Write(" " + D[r, c]);
+                }
+                Console.Write("\n");
+            }
 
-            // TO-DO
-            /* 
-                Lastly, do your outputs. 
-                (1) Output the final D matrix which will give you the lengths of the short est paths between each pair of vertices. 
-                (2) Output the path matrix P. (3) Output all the shortest paths, utilizing Algorithm 3.5 on page 109 (as modified). 
-                    Your function will call the path function. 
-             */
+            // Outputs the inital P matrix
+            Console.WriteLine("\nInital P Matrix: ");
+            for (int r = 0; r < numVertex; r++)
+            {
+                for (int c = 0; c < numVertex; c++)
+                {
+                    Console.Write(" " + P[r, c]);
+                }
+                Console.Write("\n");
+            }
+
+            // P matrix after path() 
+            Console.WriteLine("\nPath P Matrix: ");
+            for (int r = 0; r < numVertex; r++)
+            {
+                for (int c = 0; c < numVertex; c++)
+                {
+                    path(r, c);
+                }
+                Console.Write("\n");
+            }
 
         }
 
@@ -84,41 +118,50 @@ namespace GraphFloyd
          *      vertex on the shortest path from p to r. 
          *  P is kept as -1 otherwise.
          * 
-         * @author      Sarah Stepak
+         * @author      Dusty Stepak
          * @param q     int     Used as vertex
          * @param r     int     Used as vertex
          * @return      void
          */
-        public static void path(int q, int r) 
+        public static void path(int q, int r)
         {
-            if(P[q,r] != -1) 
+            if (P[q, r] != -1)
             {
                 path(q, P[q, r]);
+                Console.Write(" v" + P[q, r] + "  ");
                 path(P[q, r], r);
             }
+
         }
 
 
         /*
          * Prints the number of vertices as well as the adjacency matrix.
          *
-         * @author  Sarah Stepak
+         * @author  Dusty Stepak
          * @return  void
          */
-        public static void printGraph() 
+        public static void printGraph()
         {
-            // Prints the adjacency array
-            for (int r = 0; r < adjacency.GetLength(0); r++) 
+            // Prints the number of verticies 
+            Console.WriteLine("Number of Verticies: " + numVertex);
+
+            // Prints the adjacency rray
+            Console.WriteLine("\nThe adjacency array:");
+            for (int r = 0; r < numVertex; r++)
             {
-                for(int c = 0; c < adjacency.GetLength(1); c++) 
+                for (int c = 0; c < numVertex; c++)
                 {
-                    Console.Write(" "+ adjacency[r,c] + ",");
+                    if(adjacency[r, c] == INFINITY)
+                        Console.Write(" oo");
+                    else
+                        Console.Write(" " + adjacency[r, c]);
                 }
                 Console.Write("\n");
             }
 
-            // Prints the number of verticies 
-            Console.WriteLine("Number of Verticies: " + numVertex);
+            // Prints the Key
+            Console.WriteLine("Where oo is equal to: " + INFINITY);
 
         }
 
@@ -128,65 +171,22 @@ namespace GraphFloyd
          * appropiate values to global variables: adjacency[,] and numVertex.
          * 
          * @author  Dr. Gail Lange
-         * @author  Sarah Stepak
+         * @author  Dusty Stepak
          * @param   rdr     StreamReader    contains data from the file
          * @return  void
-         * 
-         * @see https://drive.google.com/drive/u/2/folders/13HRrYGaF3roY23DmgDKvrarMGzrW8lcA
-         * 
          */
         public static void createGraph(StreamReader rdr)
         {
             string line;
             string[] s; // for split  
-            int i, j;
 
             try
             {
                 // get the data from the file and put it into variables
                 int k = 0;
-                i = 0; j = 0;
 
-                using(rdr)
-                { 
-                    // parses through the data file
-                    while(rdr.EndOfStream == false) 
-                    {
-                        // Reads and splits the current line in the file
-                        line = rdr.ReadLine();
-                        s = line.Split(' ');                        
-
-
-                        //Ignores line 1
-                        if (s[0] == "D" || s[0] == "U") { k++; continue; } 
-
-                        // Converts string to int, and initalizes numVertex to the given number of vertices
-                        else if (k == 1)
-                        {  
-                            numVertex = Int32.Parse(s[0]);
-                            k++; continue; }
-
-                        //Ignores line 3
-                        else if (k == 2) { k++; continue;}
-
-                        else
-                        {
-                            // Checks to see if the next line is the last line. If so, sets j to length n
-                            if (rdr.Peek() == -1) { j = s.Length; continue; }
-
-                            // Counts total number of rows
-                            else { i++; continue; }
-                        }
-                    }
-
-                    // Declares adjacency array length
-                    adjacency = new int[i, j];
-
-                    // resets rdr's stream back to the top of the file
-                    rdr.DiscardBufferedData();
-                    rdr.BaseStream.Seek(0, System.IO.SeekOrigin.Begin);
-                    i = 0;  j = 0;  k = 0; // resets i,j,k
-
+                using (rdr)
+                {
                     // parses through the data file
                     while (rdr.EndOfStream == false)
                     {
@@ -194,24 +194,46 @@ namespace GraphFloyd
                         line = rdr.ReadLine();
                         s = line.Split(' ');
 
-                        // ignores the first 3 lines
-                        if (k < 3) { k++; continue; }
-                        
+
+                        //Ignores line 1
+                        if (s[0] == "D" || s[0] == "U") { k++; continue; }
+
+                        // Converts string to int, and initalizes numVertex to the given number of vertices
+                        // Initalizes adjacency array to the size of numVertex as well.
+                        // Sets non declared numbers in the adjacency array to infinity.
+                        else if (k == 1)
+                        {
+                            numVertex = Int32.Parse(s[0]);
+                            adjacency = new int[numVertex, numVertex];
+                            // Sets adjacency to initally be 0
+                            for (int r = 0; r < numVertex; r++)
+                                for (int c = 0; c < numVertex; c++)
+                                    adjacency[r, c] = 0;
+                            k++; continue; //next line
+                        }
+
+                        //Ignores line 3
+                        else if (k == 2) { k++; continue; }
+
                         else
                         {
-                            // checks to make sure we are not on the last line of the file
-                            if (rdr.Peek() != -1)
+                            // Checks to see if the current line is not the last line. If this is the case, then
+                            // the values in the adjacency array are updated to those in the data file.
+                            if (rdr.Peek() != -1) 
                             {
-                                // fills the adjacency array
-                                for (j = 0; j < adjacency.GetLength(1); j++)
-                                {
-                                    adjacency[i, j] = Convert.ToInt32(s[j]);
-                                }
-
-                                i++; continue; // goes to the next line
+                                int x, y, result;
+                                x = Int32.Parse(s[0]); y = Int32.Parse(s[1]); result = Int32.Parse(s[2]);
+                                adjacency[x, y] = result;
+                                continue; 
                             }
                         }
                     }
+
+                    // Replaces 0 with oo, if there is a zero not on the diagnol
+                    for (int i = 0; i < numVertex; i++)
+                        for (int j = 0; j < numVertex; j++)
+                            if (i != j && adjacency[i, j] == 0)
+                                adjacency[i, j] = INFINITY;
 
                 }
                 rdr.Close();
